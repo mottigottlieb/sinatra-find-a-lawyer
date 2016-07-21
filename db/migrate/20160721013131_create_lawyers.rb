c663fc226e4e7e41d528af002df2d3f0f14369c6
Motti Mordechay Gottlieb <mottigottlieb@gmail.com>
2016-07-21 01:31:53 +0000

    added migration files

---
+++ db/migrate/20160721013131_create_lawyers.rb
@@ -0,0 +1,10 @@
+class CreateLawyers < ActiveRecord::Migration
+  def change
+    create_table :lawyers do |t|
+     t.string  "name"
+     t.string  "practice"
+     t.string  "rating"
+     t.integer "years"
+     t.string  "number"
+  end
+end
