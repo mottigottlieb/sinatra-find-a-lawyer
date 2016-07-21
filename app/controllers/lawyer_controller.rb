96ca310207c6d34ffa964068fb2cbaff04393dc3
Motti Mordechay Gottlieb <mottigottlieb@gmail.com>
2016-07-21 15:22:45 +0000

    Lawyer_search Renders

--- app/controllers/lawyer_controller.rb
+++ app/controllers/lawyer_controller.rb
@@ -5,10 +5,18 @@ require 'pry'
 
 class LawyerController < ApplicationController
 
+  get "/:user/lawyer_search" do
+    erb :"lawyers/lawyer_search"
+  end
+
+  post "/lawyer_search" do
+    "Searching..."
+  end
   get '/search_for_a_lawyer' do
     @new_scrape = Scrape.new
     @new_scrape.scrape_page("https://www.avvo.com/search/lawyer_search?utf8=%E2%9C%93&q=traffic&loc=11225&button=")
     "hello"
   end
+
 
 end
