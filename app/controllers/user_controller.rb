90b7b7ee300ade363827915015c0fd3ffd6b3063
Motti Mordechay Gottlieb <mottigottlieb@gmail.com>
2016-07-21 14:18:44 +0000

    User saving post signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,4 +9,19 @@ get "/login" do
 end
 
 
+
+######## Post requests
+
+post "/signup" do
+  # Create new user
+  user = User.create(:username => params[:username], :password => params[:password])
+  if user.save
+    #set session to this user
+    session[:user_id] = user.id
+    "saved"
+  else
+    "not saved"
+  end
+
+end
 end
